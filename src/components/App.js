7ca2ff5bfdb92c7ea4dfa7f7df4328f8d4b1cabf
silulitgithub <kumarsilu530@gmail.com>
2023-12-14 23:41:45 +0530

    submitting code-Thu Dec 14 2023 23:41:43 GMT+0530 (India Standard Time)

--- src/components/App.js
+++ src/components/App.js
@@ -3,7 +3,9 @@ import '../styles/App.css';
 
 const App = () => {
   return (
-    <div id="main"></div>
+    <div id="main">
+      <p><h1>I am learning React. My life is getting better.</h1></p>
+    </div>
   )
 }
 
